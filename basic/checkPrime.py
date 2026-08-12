1be86ed9a7cf348158aca36e2706e427b1a9bd9e
Debraj Das <debrajdas.dgr@gmail.com>
2026-08-12 18:31:09 +0600

    ADD Check prime number

---
+++ basic/checkPrime.py
@@ -0,0 +1,16 @@
+import math
+num = int(input("enter the any number: "))
+
+if num <= 1:
+  print(num, "number is not prime")
+
+else:
+    is_prime = True
+    for i in range(2, int(math.sqrt(num))+1):
+       if num %i == 0:
+          is_prime = False
+          break
+       if is_prime:
+          print(num, "is a prime number")
+       else:
+          print(num, "is not a prime no")   
