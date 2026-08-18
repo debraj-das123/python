7d76348bcd82101530d87cdc70b7fae147b61323
Debraj Das <debrajdas.dgr@gmail.com>
2026-08-18 19:21:49 +0600

    ADD POsitive and Negative a Number

---
+++ basic/PositiveNegative.py
@@ -0,0 +1,7 @@
+nums = int(input("enter the any numbers: "))
+if nums > 0 :
+    print(nums, "positive number")
+elif nums <0 :
+     print(nums, "is a negative number")
+else:
+  print(nums, "is a zero")         
