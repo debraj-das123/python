080d355e5a71fd8f92522e4748b3fd0ba646dc5d
Debraj Das <debrajdas.dgr@gmail.com>
2026-09-07 20:11:13 +0600

    ADD pattern4

---
+++ pattern/pattern4.py
@@ -0,0 +1,13 @@
+n = int(input("Eneter a number a array: "))
+
+for i in range(n, 0, -1):
+    for j in range(1, i+1, 1):
+        print("*", end=" ")
+    print()    
+
+
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
